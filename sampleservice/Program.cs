ac70b29c926f4fa91c59b81ebedd4d26f09c84db
guru.seetharaman <guru.seetharaman@lightstream.com>
2021-08-14 13:20:54 -0700

    chaged program.cs

--- sampleservice/Program.cs
+++ sampleservice/Program.cs
@@ -6,7 +6,7 @@ namespace sampleservice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World.This is sampleservice app");
         }
     }
 }
